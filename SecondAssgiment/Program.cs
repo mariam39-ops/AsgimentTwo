240488a0299870d52719b43ee9bd705014518a39
Mariam <mariamahmedmostafa18@gmail.com>
2026-09-04 21:56:52 +0300

    answer 1

--- SecondAssgiment/Program.cs
+++ SecondAssgiment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            //answer1
+            int Pages = 464;
+            bool isAvailabel = true;
+            if (Pages > 300 && isAvailabel == true)
+            {
+
+                Console.WriteLine("You can borrow this book");
+            }
+            else
+            {
+                Console.WriteLine("no you can't borrow this book");
+            }
         }
     }
 }
